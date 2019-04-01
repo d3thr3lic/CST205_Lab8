3586a3e66834a663470e929996116bf8651231c9
Nick Saunders <nicholas3000@sbcglobal.net>
2019-03-31 18:13:50 -0700

    comments made and is cleaned up. Finished

--- lab8.py
+++ lab8.py
@@ -1,11 +1,20 @@
+###CST205 -- Lab8
+#Mitchell Saunders
+#Nicholas Saunders
+
 def pickAndExplore():
   clip = makeSound(pickAFile())
-  #play(clip)
+  ###Uncomment any of the functions below
+  ###to use any function.
+  
+  #play(clip) # no modifications
+  
+  #maxVolume(clip)
   #decreaseVolume(clip)
   #changeVolume(clip, 2)
   #print maxSample(clip)
-  #clip = maxVolume(clip)
-  goToEleven(clip)
+  #maxVolume(clip)
+  #goToEleven(clip)
   explore(clip)
 
 def maxVolume(sound):
@@ -19,7 +28,8 @@ def maxVolume(sound):
     currentValue = getSampleValue(sample)
     setSampleValue(sample, currentValue * factor)
   return sound
-  
+
+#takes sample values from source, and sets sampleValues to max or min.     
 def goToEleven(sound):
   maxPossibleSampleValue = 127
   for sample in getSamples(sound):
@@ -28,6 +38,7 @@ def goToEleven(sound):
     else:
       setSampleValue(sample, maxPossibleSampleValue * -1)
 
+#sets sound to max sound sample value.
 def maxSample(sound):
   runningMax = 0;
   for sample in getSamples(sound):
@@ -35,17 +46,20 @@ def maxSample(sound):
     if localMax > runningMax:
       runningMax = localMax
   return runningMax
-  
+
+#if called, will print to value of the sample of a clip    
 def printSample():
   clip = makeSound(pickAFile())
   #sample = getSamples(clip)
   print getSampleValueAt(clip, 10000)
 
+#decreases volume of sound by half
 def decreaseVolume(sound):
    for sample in getSamples(sound):
       value = getSampleValue(sample)
       setSampleValue(sample, value / 2)
-      
+
+#like decreaseVolume, but it modifable by a factor parameter.            
 def changeVolume(sound, factor):
    for sample in getSamples(sound):
       value = getSampleValue(sample)
